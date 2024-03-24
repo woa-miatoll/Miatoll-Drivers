ef89b7200c14572ab4139b83d123335db552ae01
N1kroks <nikroksm@mail.ru>
2024-03-24 12:25:01 +0500

    Remove generic dcs backlight & enable GPU

--- tools/pack-miatoll.cmd
+++ tools/pack-miatoll.cmd
@@ -19,6 +19,23 @@ xcopy /cheriky ..\components\QC7125 miatoll-Drivers-Full\components\QC7125
 xcopy /cheriky ..\components\ANYSOC miatoll-Drivers-Full\components\ANYSOC
 xcopy /cheriky ..\definitions\Desktop miatoll-Drivers-Full\definitions\Desktop
 
+copy .\DriverUpdater.ARM64.exe miatoll-Drivers-Full
+copy .\DriverUpdater.AMD64.exe miatoll-Drivers-Full
+copy .\DriverUpdater.X86.exe miatoll-Drivers-Full
+
+echo @echo off > miatoll-Drivers-Full\OnlineUpdater.cmd
+echo ^(NET FILE^|^|^(powershell -command Start-Process '%%0' -Verb runAs -ArgumentList '%%* '^&EXIT /B^)^)^>NUL 2^>^&1 >> miatoll-Drivers-Full\OnlineUpdater.cmd
+echo pushd "%%~dp0" ^&^& cd %%~dp0 >> miatoll-Drivers-Full\OnlineUpdater.cmd
+echo DriverUpdater.%%PROCESSOR_ARCHITECTURE%%.exe -r . -d .\definitions\Desktop\ARM64\Internal\miatoll.xml >> miatoll-Drivers-Full\OnlineUpdater.cmd
+echo pause >> miatoll-Drivers-Full\OnlineUpdater.cmd
+
+echo @echo off > miatoll-Drivers-Full\OfflineUpdater.cmd
+echo ^(NET FILE^|^|^(powershell -command Start-Process '%%0' -Verb runAs -ArgumentList '%%* '^&EXIT /B^)^)^>NUL 2^>^&1 >> miatoll-Drivers-Full\OfflineUpdater.cmd
+echo pushd "%%~dp0" ^&^& cd %%~dp0 >> miatoll-Drivers-Full\OfflineUpdater.cmd
+echo set /P DrivePath=Enter Drive letter ^^^(with the colon!^^^) of the connected device in mass storage mode ^^^(e.g. D:^^^): >> miatoll-Drivers-Full\OfflineUpdater.cmd
+echo DriverUpdater.%%PROCESSOR_ARCHITECTURE%%.exe -r . -d .\definitions\Desktop\ARM64\Internal\miatoll.xml -p %%DrivePath%% >> miatoll-Drivers-Full\OfflineUpdater.cmd
+echo pause >> miatoll-Drivers-Full\OfflineUpdater.cmd
+
 "%ProgramFiles%\7-zip\7z.exe" a -tzip -r ..\..\miatoll-Drivers-Release\miatoll-Drivers-Full.zip miatoll-Drivers-Full\*
 
 REM move miatoll-Drivers-Full\components\QC7125 ..\components\QC7125

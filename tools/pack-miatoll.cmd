a021ba7785917b371de9f482d7c9c078104f08a9
N1kroks <nikroksm@mail.ru>
2024-06-05 00:45:10 +0500

    Update pack script'

--- tools/pack-miatoll.cmd
+++ tools/pack-miatoll.cmd
@@ -13,6 +13,7 @@ echo @echo off > ..\OfflineUpdater.cmd
 echo ^(NET FILE^|^|^(powershell -command Start-Process '%%0' -Verb runAs -ArgumentList '%%* '^&EXIT /B^)^)^>NUL 2^>^&1 >> ..\OfflineUpdater.cmd
 echo pushd "%%~dp0" ^&^& cd %%~dp0 >> ..\OfflineUpdater.cmd
 echo set /P DrivePath=Enter Drive letter ^^^(with the colon!^^^) of the connected device in mass storage mode ^^^(e.g. X:^^^): >> ..\OfflineUpdater.cmd
+echo if not "%%DrivePath:~1,1%%"==":" set DrivePath=%%DrivePath%%: >> ..\OfflineUpdater.cmd
 echo .\tools\DriverUpdater\%%PROCESSOR_ARCHITECTURE%%\DriverUpdater.exe -r . -d .\definitions\Desktop\ARM64\Internal\miatoll.xml -p %%DrivePath%% >> ..\OfflineUpdater.cmd
 echo pause >> ..\OfflineUpdater.cmd
 
@@ -21,13 +22,12 @@ echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.BASE >> filelist_miatoll.
 echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.MOBILE_COMPONENTS >> filelist_miatoll.txt
 echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.MOBILE_RIL >> filelist_miatoll.txt
 echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.MOBILE_RIL_EXTRAS >> filelist_miatoll.txt
-echo components\QC7125\Device\Miatoll\DEVICE.SOC_QC7125.MIATOLL >> filelist_miatoll.txt
-echo components\QC7125\Device\Miatoll\DEVICE.SOC_QC7125.MIATOLL_MINIMAL >> filelist_miatoll.txt
-echo components\QC7125\Device\Miatoll\GRAPHICS.SOC_QC7125.MIATOLL_DESKTOP >> filelist_miatoll.txt
+echo components\QC7125\Device\DEVICE.SOC_QC7125.MIATOLL >> filelist_miatoll.txt
+echo components\QC7125\Device\DEVICE.SOC_QC7125.MIATOLL_MINIMAL >> filelist_miatoll.txt
+echo components\QC7125\GRAPHICS\GRAPHICS.SOC_QC7125.MIATOLL_DESKTOP >> filelist_miatoll.txt
 echo components\QC7125\Platform\PLATFORM.SOC_QC7125.BASE >> filelist_miatoll.txt
 echo components\QC7125\Platform\PLATFORM.SOC_QC7125.BASE_MINIMAL >> filelist_miatoll.txt
 echo definitions\Desktop\ARM64\Internal\miatoll.xml >> filelist_miatoll.txt
-echo definitions\Desktop\ARM64\PE\miatoll.xml >> filelist_miatoll.txt
 echo tools\DriverUpdater >> filelist_miatoll.txt
 echo OfflineUpdater.cmd >> filelist_miatoll.txt
 echo OnlineUpdater.cmd >> filelist_miatoll.txt
